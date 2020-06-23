d4fd868c08ec2e4fc5468ed32227fea4ae33eb52
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-06-23 20:05:32 +0530

    Todo list App and removed few lines in fetch

---
+++ TodoList/todo.js
@@ -0,0 +1,27 @@
+function todos() {
+    return {
+        todos: [{
+            id: 1,
+            title: 'Finish Alpine',
+            isComplete: false
+        }],
+        todoTitle:' ',
+        todoId: 2,
+        todoDate: " ",
+        addTodo(){
+            if(this.todoTitle === " "){
+                return;
+            }
+            this.todos.push({
+                    id:this.todoId,
+                    title: this.todoTitle.substr(0 ,2).toUpperCase() + this.todoTitle.substring(2),
+                    isComplete: false,
+                });
+            this.todoId++;
+            this.todoTitle = " ";
+        },
+        deleteTodo(id){
+            this.todos = this.todos.filter(todo => id!== todo.id);
+        },
+    }
+}
